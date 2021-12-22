e1c5c6eac82b8c57dcc5d78e2e77f544d3d9f436
Giulio Lelis <giuliolsc1405@gmail.com>
2021-12-21 22:41:32 -0300

    Opcionais 3, 4 e 5

--- tarefas.js
+++ tarefas.js
@@ -2,6 +2,7 @@ const listaTarefasEl = document.querySelector('#lista-tarefas');
 const botaoIncluirNovaTarefaEl = document.querySelector('#incluir-nova-tarefa');
 const inputNovaTarefaNomeEl = document.querySelector('#nova-tarefa-nome');
 const selectNovaTarefaCategoriaEl = document.querySelector('#nova-tarefa-categoria');
+let itensTarefa = document.querySelectorAll('.item-tarefa');
 
 function Tarefa(nome, categoria, realizada) {
     this.nome = nome;
@@ -11,20 +12,44 @@ function Tarefa(nome, categoria, realizada) {
 
 const tarefas = [new Tarefa('Comprar leite', 'compras', false), new Tarefa('Escutar chimbinha', 'lazer', true)];
 
-function insereTarefaNaPagina(tarefa) {
-    console.log({tarefa})
-    listaTarefasEl.innerHTML += `<li class="item-tarefa categoria-${tarefa.categoria}${tarefa.realizada ? ' marcado' : ''}">${tarefa.nome}</li>`;
+insereTarefasNaPagina(selectNovaTarefaCategoriaEl.value);
+
+function insereTarefaNaPagina(tarefa, categoriaSelecionada = null) {
+    listaTarefasEl.innerHTML += `
+        <li class="item-tarefa categoria-${tarefa.categoria} ${tarefa.realizada ? 'marcado' : ''} ${tarefa.categoria !== categoriaSelecionada ? 'retido-no-filtro' : ''}">${tarefa.nome}</li>
+    `;
 }
 
-listaTarefasEl.innerHTML = '';
-tarefas.forEach(insereTarefaNaPagina);
+function insereTarefasNaPagina(categoriaSelecionada = null) {
+    listaTarefasEl.innerHTML = '';
+    tarefas.forEach((tarefa) => insereTarefaNaPagina(tarefa, categoriaSelecionada));
+    itensTarefa = document.querySelectorAll('.item-tarefa');
+}
 
-
-botaoIncluirNovaTarefaEl.addEventListener("click", function(e) {
+function insereNovaTarefa(e) {
     const nome = inputNovaTarefaNomeEl.value;
     const categoria = selectNovaTarefaCategoriaEl.value;
     const novaTarefa = new Tarefa(nome, categoria, false);
-    insereTarefaNaPagina(novaTarefa);
+    tarefas.push(novaTarefa);
+    insereTarefaNaPagina(novaTarefa, selectNovaTarefaCategoriaEl.value);
     inputNovaTarefaNomeEl.value = '';
     inputNovaTarefaNomeEl.focus();
+}
+
+selectNovaTarefaCategoriaEl.addEventListener("change", function(e) {
+    insereTarefasNaPagina(e.target.value);
+});
+
+botaoIncluirNovaTarefaEl.addEventListener("click", insereNovaTarefa);
+
+inputNovaTarefaNomeEl.addEventListener("keyup", function(e) {
+    if(e.key === 'Enter') insereNovaTarefa();
+});
+
+itensTarefa.forEach((itemTarefa) => {
+    itemTarefa.addEventListener("click", function(e) {
+        const indexItemTarefa = tarefas.findIndex((tarefa) => tarefa.nome === itemTarefa.innerHTML);
+        tarefas[indexItemTarefa].realizada = !tarefas[indexItemTarefa].realizada;
+        itemTarefa.classList.toggle('marcado');
+    });
 });
